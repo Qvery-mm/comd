1ea53350be8e1cd6fbda6d766b002e9a5837085b
cool-school <aleksandr.kovalchukov@mail.ru>
2017-07-08 11:10:26 +0300

    Test version

---
+++ detection.py
@@ -0,0 +1,73 @@
+from PIL import Image, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+from convert import *
+
+
+def settings(image, c=2, e=0.5):
+    copy = ImageEnhance.Brightness(image).enhance(e)
+    copy = ImageEnhance.Contrast(copy).enhance(c)
+    return copy
+
+
+def cmp_(x, y):
+    return x[1] > y[1]
+
+
+def histogram(image):
+    y = image.histogram()
+    MAX = []
+    for i in range(256):
+        if y[i] != 0:
+            MAX.append((i, y[i]))
+    MAX.sort(key=lambda x: x[1], reverse=True)
+    print(MAX)
+    return MAX
+
+
+def showImg(arr):
+    plt.imshow(arr, cmap='gray')
+    arr = np.flip(arr, 0)
+    plt.contour(arr, levels=[0], colors='white', origin='image')
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    im = Image.open(buf)
+    im.convert("L")
+    im.show()
+    buf.close()
+    return im
+
+
+def get_points(image):
+    return (70, 65, 430, 300) # x1, y1, x2, y2
+
+
+def detect(fname):
+    im = init(fname)
+    copy = im.crop(get_points(im))
+    copy = settings(copy, 6, 0.5)
+    while (histogram(copy)[0][0] != 255) and (histogram(copy)[0][0] != 0):
+        copy = settings(copy, 1.2, 1)
+    return copy
+
+def light(copy, im):
+    t = get_points(im)
+    arr = np.array(copy)
+    output_arr = np.array(im)
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            output_arr[i + t[1]][j + t[0]][0] = arr[i][j]
+            output_arr[i + t[1]][j + t[0]][1] += arr[i][j]
+
+    im = Image.fromarray(output_arr)
+    im.show()
+    return im
+
+
+if __name__ == "__main__":
+    fname = "37.png"
+    im = Image.open(fname)
+    #showImg(detect(fname))
+    light(detect(fname), im).save("output.png")
